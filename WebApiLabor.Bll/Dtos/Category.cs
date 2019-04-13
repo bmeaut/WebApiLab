42dd3b0b0b70be3d0651d0555becbd8bf1b0bbe3
kszicsillag <kszicsillag@gmail.com>
2019-04-13 20:13:54 +0200

    fixes

--- WebApiLabor.Bll/Dtos/Category.cs
+++ WebApiLabor.Bll/Dtos/Category.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApiLabor.Api.Dtos
+﻿namespace WebApiLabor.Api.Dtos
 {
     public class Category
     {
--- WebApiLabor.Bll/Dtos/Product.cs
+++ WebApiLabor.Bll/Dtos/Product.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using WebApiLabor.Bll.Entities;
+﻿using System.Collections.Generic;
+using WebApiLabor.Entities;
 
 namespace WebApiLabor.Api.Dtos
 {
