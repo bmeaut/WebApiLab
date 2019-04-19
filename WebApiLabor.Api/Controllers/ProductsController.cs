5578f8b7cc4542b4d63fb8164d9c99415bbfbe9c
kszicsillag <kszicsillag@gmail.com>
2019-04-19 22:39:00 +0200

    prodcontroller using service +automapper

---
+++ WebApiLabor.Api/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiLabor.Bll.Services;
+using WebApiLabor.Entities;
+
+namespace WebApiLabor.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+       
+
+
+        // GET: api/Products
+        //[HttpGet]
+        //public IEnumerable<Product> Get()
+        //{
+        //    return _productService.GetProducts();
+        //}
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return _productService.GetProducts()
+                .ToList();
+        }       
+
+        // GET: api/Products/5
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<Product> Get(int id)
+        {
+            return _productService.GetProduct(id);
+        }
+
+        // POST: api/Products
+        [HttpPost]
+        public ActionResult<Product> Post([FromBody] Product product)
+        {
+            var created = _productService.InsertProduct(product);
+            return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
+        }
+
+        // PUT: api/Products/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Product product)
+        {
+            _productService.UpdateProduct(id, product);
+            return NoContent();
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _productService.DeleteProduct(id);
+            return NoContent();
+        }
+    }
+}
--- WebApiLabor.Api/Startup.cs
+++ WebApiLabor.Api/Startup.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using WebApiLabor.Bll.Services;
 using WebApiLabor.DAL;
 
 namespace WebApiLabor.Api
@@ -21,11 +25,29 @@ namespace WebApiLabor.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddJsonOptions(
+                    json => json.SerializerSettings.ReferenceLoopHandling 
+                            = ReferenceLoopHandling.Ignore);
 
             services.AddDbContext<NorthwindContext>(o =>
                 o.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"])
                 .ConfigureWarnings(c => c.Throw(RelationalEventId.QueryClientEvaluationWarning)));
+
+            services.AddTransient<IProductService, ProductService>();
+
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.CreateMap<Entities.Product, Dtos.Product>()
+                    .ForMember(dto => dto.Orders, opt => opt.Ignore())
+                    .AfterMap((p, dto, ctx) =>
+                        dto.Orders = p.ProductOrders.Select(po =>
+                        ctx.Mapper.Map<Dtos.Order>(po.Order)).ToList()).ReverseMap();                    
+                cfg.CreateMap<Entities.Order, Dtos.Order>().ReverseMap();                    
+                cfg.CreateMap<Entities.Category, Dtos.Category>().ReverseMap();
+            }
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
