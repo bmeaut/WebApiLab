fa214625211c82f4342edd7cb11638377b346c33
kszicsillag <kszicsillag@gmail.com>
2019-04-28 15:27:54 +0200

    client proj

---
+++ WebApiLabor.Client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiLabor.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("ProductId: ");
+            var id = Console.ReadLine();
+            await GetProductAsync(Int32.Parse(id));
+
+            Console.ReadKey();
+        }
+
+        public static async Task GetProductAsync(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                /*Port-ot írjuk át a szervernek megfelelően*/
+                var response = await client.
+                    GetAsync(new Uri($"http://localhost:5000/api/Products/{id}"));
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(json);
+                }
+            }
+        }
+    }
+}
