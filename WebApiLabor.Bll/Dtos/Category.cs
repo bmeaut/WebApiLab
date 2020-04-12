e1b8171f9340771bd8866ced767e57f2760a94fc
kszicsillag <kszicsillag@gmail.com>
2020-04-12 13:29:16 +0200

    clear

--- WebApiLabor.Bll/Dtos/Category.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApiLabor.Api.Dtos
-{
-    public class Category
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- WebApiLabor.Bll/Services/IOrderService.cs
+++
@@ -1,9 +0,0 @@
-﻿using WebApiLabor.Entities;
-
-namespace WebApiLabor.Bll.Services
-{
-    public interface IOrderService
-    {
-        Order CreateOrder(int productId, Order order);
-    }
-}
