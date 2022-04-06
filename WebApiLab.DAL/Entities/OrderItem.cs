d532092b16f2e11bb2e6238434aa23b2aa041e08
Tóth Tibor <toth.tibor@autsoft.hu>
2022-04-06 10:21:08 +0200

    nav propertyk nem nullozhatóak

--- WebApiLab.DAL/Entities/OrderItem.cs
+++ WebApiLab.DAL/Entities/OrderItem.cs
@@ -6,11 +6,11 @@ public class OrderItem
 
     public int ProductId { get; set; }
 
-    public Product? Product { get; set; } = null!;
+    public Product Product { get; set; } = null!;
 
     public int OrderId { get; set; }
 
-    public Order? Order { get; set; } = null!;
+    public Order Order { get; set; } = null!;
 
     public int Quantity { get; set; }
 
--- WebApiLab.DAL/Entities/Product.cs
+++ WebApiLab.DAL/Entities/Product.cs
@@ -12,7 +12,8 @@ public class Product
     public int UnitPrice { get; set; }
 
     public int CategoryId { get; set; }
-    public Category? Category { get; set; }
+
+    public Category Category { get; set; } = null!;
 
     public ICollection<OrderItem> ProductOrders { get; } = new List<OrderItem>();
 
