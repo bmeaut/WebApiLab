18914d087e0d7cd7e0d8d3875b0228505d474ba4
Tóth Tibor <toth.tibor@autsoft.hu>
2022-04-21 10:48:04 +0200

    Console app kliens kézzel

---
+++ WebApiLab.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+Console.Write("ProductId: ");
+var id = Console.ReadLine();
+await GetProductAsync(int.Parse(id));
+
+Console.ReadKey();
+
+static async Task GetProductAsync(int id)
+{
+    using var client = new HttpClient();
+
+    /*A portot írjuk át a szervernek megfelelően*/
+    var response = await client.GetAsync(new Uri($"http://localhost:5184/api/Products/{id}"));
+    response.EnsureSuccessStatusCode();
+    var jsonStream = await response.Content.ReadAsStreamAsync();
+    var json = await JsonDocument.ParseAsync(jsonStream);
+    Console.WriteLine($"{json.RootElement.GetProperty("name")}:" +
+        $"{json.RootElement.GetProperty("unitPrice")}.-");
+}
