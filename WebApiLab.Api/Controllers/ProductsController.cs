7ac01c4ef0675d50baa430d88f0f096ffb3ba38e
Simon Gábor <simon.gabor@vik.bme.hu>
2023-05-05 12:56:54 +0200

    Async controller methods rename

--- WebApiLab.Api/Controllers/ProductsController.cs
+++ WebApiLab.Api/Controllers/ProductsController.cs
@@ -18,29 +18,29 @@ public class ProductsController : ControllerBase
 
     // GET: api/<ProductsController>
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Product>>> GetAsync()
+    public async Task<ActionResult<IEnumerable<Product>>> Get()
     {
         return (await _productService.GetProductsAsync()).ToList();
     }
 
     // GET api/<ProductsController>/5
     [HttpGet("{id}")]
-    public async Task<ActionResult<Product>> GetAsync(int id)
+    public async Task<ActionResult<Product>> Get(int id)
     {
         return await _productService.GetProductAsync(id);
     }
 
     // POST api/<ProductsController>
     [HttpPost]
-    public async Task<ActionResult<Product>> PostAsync([FromBody] Product product)
+    public async Task<ActionResult<Product>> Post([FromBody] Product product)
     {
         var created = await _productService.InsertProductAsync(product);
-        return CreatedAtAction(nameof(GetAsync), new { id = created.Id }, created);
+        return CreatedAtAction(nameof(Get), new { id = created.Id }, created);
     }
 
     // PUT api/<ProductsController>/5
     [HttpPut("{id}")]
-    public async Task<ActionResult> PutAsync(int id, [FromBody] Product value)
+    public async Task<ActionResult> Put(int id, [FromBody] Product value)
     {
         await _productService.UpdateProductAsync(id, value);
         return NoContent();
@@ -48,7 +48,7 @@ public class ProductsController : ControllerBase
 
     // DELETE api/<ProductsController>/5
     [HttpDelete("{id}")]
-    public async Task<ActionResult> DeleteAsync(int id)
+    public async Task<ActionResult> Delete(int id)
     {
         await _productService.DeleteProductAsync(id);
         return NoContent();
